47c1fbf1e5b42c1c5d1b407b842a38f4c2fc2760
Alexandru Stefanescu <alexandru.stefanescu@3pillar.corp>
2017-06-19 10:29:29 +0300

    Dictionaries sort

--- dict_sort.py
+++ dict_sort.py
@@ -3,9 +3,7 @@ import os.path
 
 
 def read_dicts(file):
-    dictionaries=[]
-    if not os.path.exists(file):
-        return False
+    dictionaries = []
     with open(file, "r") as f:
         while True:
             temp_dict = {}
@@ -13,36 +11,37 @@ def read_dicts(file):
             if not inf:
                 break
             inf = inf.rstrip("\n\r")
-            inf = str.split(inf,' ')
-            for i in range(0,len(inf)-1,2):
-                temp_dict["%s".format(inf[i])] = inf[i+1]
+            inf = str.split(inf, ' ')
+            for i in range(0, len(inf)-1, 2):
+                temp_dict[format(inf[i])] = inf[i+1]
             dictionaries.append(temp_dict)
     return dictionaries
 
 
 def sort_dicts(input_file):
     sorting = True
-    dictionaries = read_dicts(input_file);
-    if dictionaries == False:
-        print("Failed to open file");
-        return False;
+    if not os.path.exists(input_file):
+        return False
+    dicts = read_dicts(input_file)
     while sorting:
         sorting = False
-        for x in range(len(dictionaries)-1):
-            if dictionaries[x][min(dictionaries[x])] > dictionaries[x+1][min(dictionaries[x+1])]:
-                dictionaries[x], dictionaries[x+1] = dictionaries[x+1],dictionaries[x]
+        for x in range(len(dicts)-1):
+            if dicts[x][min(dicts[x])] > dicts[x+1][min(dicts[x+1])]:
+                dicts[x], dicts[x+1] = dicts[x+1], dicts[x]
                 sorting = True
-    return dictionaries
+    return dicts
 
 
 class TestSort(unittest.TestCase):
     def testNoFile(self):
         self.assertEqual(sort_dicts("noFile"), False)
 
-
     def testEmptyFile(self):
         self.assertEqual(sort_dicts("emptyFile.txt"), [])
 
-
-    def simpleTest(self):
-        self.assertEqual(sort_dicts("in.txt"),[{'a': '1', 'b': '3', 'c': '4'}, {'a': '8', 'b': '2', 'c': '3'}])
+    def testSimpleList(self):
+        first_dict = {'a': '1', 'b': '3', 'c': '4'}
+        second_dict = {'a': '8', 'b': '2', 'c': '3'}
+        third_dict = {'a': '1', 'b': '1', 'c': '1'}
+        self.assertEqual(sort_dicts("in.txt"),
+                         [first_dict, third_dict, second_dict])
