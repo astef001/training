75ffd17fca15dce08808182e45037b30b57ddadf
Alexandru Stefanescu <alexandru.stefanescu@3pillar.corp>
2017-06-19 10:09:36 +0300

    Added review modifications

--- flatten/flatten.py
+++ flatten/flatten.py
@@ -1,28 +1,53 @@
 import unittest
-def flatten(list_a,list_b,max_depth):
-    list_a_flatten=[]
-    list_b_flatten=[]
-    if max_depth==0:
-        return list_a,list_b
+
+
+def flatten_list(list_a):
+    list_b = []
     for x in list_a:
-        if isinstance(x,list):
-            list_a_flatten+=x
+        if isinstance(x, list):
+            list_b.extend(x)
         else:
-            list_a_flatten+=[x]
-    for x in list_b:
-        if isinstance(x,list):
-            list_b_flatten+=x
-        else:
-            list_b_flatten+=[x]
-    return flatten(list_a_flatten,list_b_flatten,max_depth-1)
+            list_b.append(x)
+    return list_b
+
+
+def flatten(list_a, list_b, max_depth):
+    list_a_flatten = []
+    list_b_flatten = []
+    if max_depth == 0:
+        return list_a, list_b
+    list_a_flatten = flatten_list(list_a)
+    list_b_flatten = flatten_list(list_b)
+    return flatten(list_a_flatten, list_b_flatten, max_depth-1)
+
 
 class TestFlatten(unittest.TestCase):
     def test_depth_one(self):
-        self.assertEqual(flatten([1,2,[3,[4,5,6]]],[1,[[[2,3],[4,5]],8]],1),([1, 2, 3, [4, 5, 6]], [1, [[2, 3], [4, 5]], 8]))
-    def test_depth_two(self):
-        self.assertEqual(flatten([1,2,[3,[4,5,6]]],[1,[[[2,3],[4,5]],8]],2),([1, 2, 3, 4, 5, 6], [1, [2, 3], [4, 5], 8]))
-    def test_depth_three(self):
-        self.assertEqual(flatten([1, 2, [3, [4, 5, 6]]], [1, [[[2, 3], [4, 5]], 8]], 3),([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 8]))
+        list_a = [1, 2, [3, [4, 5, 6]]]
+        list_b = [1, [[[2, 3], [4, 5]], 8]]
+        result_list_a = [1, 2, 3, [4, 5, 6]]
+        result_list_b = [1, [[2, 3], [4, 5]], 8]
+        self.assertEqual(flatten(list_a, list_b, 1),
+                         (result_list_a, result_list_b))
 
-if __name__== '__main__':
+    def test_depth_two(self):
+        list_a = [1, 2, [3, [4, 5, 6]]]
+        list_b = [1, [[[2, 3], [4, 5]], 8]]
+        result_list_a = [1, 2, 3, 4, 5, 6]
+        result_list_b = [1, [2, 3], [4, 5], 8]
+        self.assertEqual(flatten(list_a, list_b, 2),
+                         (result_list_a, result_list_b))
+
+    def test_depth_three(self):
+        list_a = [1, 2, [3, [4, 5, 6]]]
+        list_b = [1, [[[2, 3], [4, 5]], 8]]
+        result_list_a = [1, 2, 3, 4, 5, 6]
+        result_list_b = [1, 2, 3, 4, 5, 8]
+        self.assertEqual(flatten(list_a, list_b, 3),
+                         (result_list_a, result_list_b))
+
+    def test_empty_list(self):
+        self.assertEqual(flatten([], [], 3), ([], []))
+
+if __name__ == '__main__':
     unittest.main()
