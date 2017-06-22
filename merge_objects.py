3179bc180cec68b2aa2bc3beb2a3be1b32c9cc77
Alexandru Stefanescu <alexandru.stefanescu@3pillar.corp>
2017-06-22 13:18:37 +0300

    Review modifications

--- merge_objects.py
+++ merge_objects.py
@@ -2,52 +2,50 @@ import unittest
 
 
 def merge(obj_a, obj_b):
+    if obj_a is None or obj_b is None:
+        return obj_a or obj_b
     if type(obj_a) != type(obj_b):
         return obj_a, obj_b
     if isinstance(obj_a, (set, frozenset)):
-        return set(obj_a | obj_b)
+        return obj_a | obj_b
     if isinstance(obj_a, dict):
-        result = {}
-        for k in (list(obj_a.keys())+list(obj_b.keys())):
-                result[k] = merge(obj_a[k], obj_b[k])
-        return result
+        return merge_dict(obj_a, obj_b)
     return obj_a + obj_b
 
 
-def main(obj_a, obj_b):
-    obj_result = {}
-    obj_result.update(obj_a)
-    for k, v in obj_b.items():
-        if k in obj_result.keys():
-            obj_result[k] = merge(obj_result[k], v)
-        else:
-            obj_result[k] = v
-    return obj_result
+def merge_dict(obj_a, obj_b):
+    result = {}
+    for k in (list(obj_a.keys()) + list(obj_b.keys())):
+        result[k] = merge(obj_a.get(k), obj_b.get(k))
+    return result
 
 
 class TestMerge(unittest.TestCase):
-    __obj_a = {'x': [1, 2, 3], 'y': 1, 'z': set([1, 2, 3]),
-               'w': 'qweqwe', 't': {'a': [1, 2]}, 'm': [1]}
+    def setUp(self):
+        self.__obj_a = {'x': [1, 2, 3], 'y': 1, 'z': set([1, 2, 3]),
+                        'w': 'qweqwe', 't': {'a': [1, 2]}, 'm': [1]}
 
-    __obj_b = {'x': [4, 5, 6],
-               'y': 4,
-               'z': set([4, 2, 3]),
-               'w': 'asdf',
-               't': {'a': [3, 2]},
-               'm': "wer"}
+        self.__obj_b = {'x': [4, 5, 6],
+                        'y': 4,
+                        'z': set([4, 2, 3]),
+                        'w': 'asdf',
+                        't': {'a': [3, 2]},
+                        'm': "wer",
+                        'different_key': "a value"}
 
-    __result = {'x': [1, 2, 3, 4, 5, 6],
-                'y': 5,
-                'z': set([1, 2, 3, 4]),
-                'w': 'qweqweasdf',
-                't': {'a': [1, 2, 3, 2]},
-                'm': ([1], "wer")}
+        self.__result = {'x': [1, 2, 3, 4, 5, 6],
+                         'y': 5,
+                         'z': set([1, 2, 3, 4]),
+                         'w': 'qweqweasdf',
+                         't': {'a': [1, 2, 3, 2]},
+                         'm': ([1], "wer"),
+                         'different_key': "a value"}
 
     def test_void_params(self):
-        self.assertEqual(main({}, {}), {})
+        self.assertEqual(merge_dict({}, {}), {})
 
     def test_one_void_param(self):
-        self.assertEqual(main(self.__obj_a, {}), self.__obj_a)
+        self.assertEqual(merge_dict(self.__obj_a, {}), self.__obj_a)
 
     def test_merge(self):
-        self.assertEqual(main(self.__obj_a, self.__obj_b), self.__result)
+        self.assertEqual(merge_dict(self.__obj_a, self.__obj_b), self.__result)
