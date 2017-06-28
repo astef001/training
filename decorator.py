e0428c23e559185f22ebeab1b45b019284e2485e
Alexandru Stefanescu <alexandru.stefanescu@3pillar.corp>
2017-06-28 12:22:28 +0300

    Added functools.wraps

--- decorator.py
+++ decorator.py
@@ -1,5 +1,5 @@
 import time
-
+from functools import wraps
 
 def get_time():
     return time.time()
@@ -7,18 +7,22 @@ def get_time():
 
 def time_slow(threshold):
     def wrapped_func(f):
-        start = get_time()
-        result=f()
-        stop = get_time()
-        if threshold and stop - start > threshold:
-            return "Threshold exceeded: {0}".format(stop - start)
-
-        print("{0} executed in {1}".format(f.__name__, stop - start))
-        return result
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            start = get_time()
+            result = f(*args, **kwargs)
+            stop = get_time()
+            if threshold and stop - start > threshold:
+                return "Threshold exceeded: {0}".format(stop - start)
+            print("{0} executed in {1}".format(f.__name__, stop - start))
+            return result
+        return wrapper
     return wrapped_func
 
 
-@time_slow(threshold=5)
+@time_slow(threshold=22)
 def test_slow():
     for i in range(0, 1000000000):
         pass
+
+print(test_slow)
